4ff0f16575ce4a4d553818721a1602754316b880
HackersHero <h13a005@gmail.com>
2023-07-20 22:16:55 +0530

    code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
-declare var $ENV: any;
+
 
 export const environment = {
   production: true,
+  backendUrl : "http://backend:8080/"
+  // backendUrl: $ENV.API_URL || "http://localhost:8080"
   // backendUrl: process.env.API_URL || "http://localhost:8080"
 };
