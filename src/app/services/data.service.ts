f5131e6470a6d5988b3e9e4248e1fbfc556a61f7
HackersHero <h13a005@gmail.com>
2023-07-24 18:38:11 +0530

    Code

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -9,15 +9,19 @@ export class DataService {
 
   private REST_API_SERVER = environment.backendUrl ;
   // private REST_API_SERVER = "http://localhost:8080";
+  private httpheadersGet = new HttpHeaders().set("Access-Control-Allow-Origin", "*");
 
-  constructor(private httpClient : HttpClient) { }
+  constructor(private httpClient : HttpClient) { 
+     
+
+  }
 
   get(url : any){
-    return this.httpClient.get(this.REST_API_SERVER + url);
+    return this.httpClient.get(this.REST_API_SERVER + url, {"headers": this.httpheadersGet});
   }
 
   post(url : string, request : any){
-    return this.httpClient.post(this.REST_API_SERVER + url, request);
+    return this.httpClient.post(this.REST_API_SERVER + url, request, {"headers": this.httpheadersGet});
   }
 
   
