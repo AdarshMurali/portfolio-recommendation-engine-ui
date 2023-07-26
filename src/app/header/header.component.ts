5960bb0c3746943764b06ee7a266cbc5f27e12e2
HackersHero <h13a005@gmail.com>
2023-07-26 21:32:33 +0530

    code

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router, NavigationStart } from '@angular/router';
+import { User } from '../shared/models/user';
+import { PortfolioRecomService } from '../portfolio-recom.service';
 
 @Component({
   selector: 'app-header',
@@ -10,10 +12,18 @@ export class HeaderComponent implements OnInit {
 
   @Input() inputFromParent : boolean = true;
 
-  constructor(private router: Router) {
+  user! : User;
+  name! : String;
+  constructor(private router: Router, private portfolioRecomService : PortfolioRecomService) {
     }
 
   ngOnInit(): void {
+    try{
+    this.user = this.portfolioRecomService.getUser();
+    this.name = this.user.firstname + " " + this.user.lastname;
+    }catch(error){
+      this.name = 'Steven Jones';
+    }
   }
 
 }
